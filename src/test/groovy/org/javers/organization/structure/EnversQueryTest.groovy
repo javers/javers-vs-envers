e57076728d16b22ec1f76934f1808013f5d328a8
Bartosz Walacik <bartosz.walacik@allegrogroup.com>
2017-10-06 18:42:37 +0200

    use cases

---
+++ src/test/groovy/org/javers/organization/structure/EnversQueryTest.groovy
@@ -0,0 +1,54 @@
+package org.javers.organization.structure
+
+import org.hibernate.envers.AuditReaderFactory
+import org.hibernate.envers.query.AuditEntity
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.transaction.Transactional
+
+import static org.hibernate.envers.RevisionType.MOD
+
+@SpringBootTest
+class EnversQueryTest extends Specification{
+    @Autowired
+    HierarchyService hierarchyService
+
+    @Autowired
+    TheCleaner theCleaner
+
+    @PersistenceContext
+    EntityManager entityManager
+
+    @Transactional
+    def "should browse Envers history of objects by type"(){
+        given:
+        def gandalf = hierarchyService.initStructure()
+        def aragorn = gandalf.getSubordinate('Aragorn')
+        gandalf.prettyPrint()
+
+        //changes
+        hierarchyService.giveRaise(gandalf, 200)
+        hierarchyService.updateCity(gandalf, 'Shire')
+        hierarchyService.giveRaise(aragorn, 100)
+        hierarchyService.updateCity(aragorn, 'Shire')
+
+        when:
+        List folks = AuditReaderFactory
+                .get(entityManager)
+                .createQuery()
+                .forRevisionsOfEntity( Employee, false, true )
+                .add(AuditEntity.revisionType().eq(MOD)) // filter out initial versions
+                .getResultList()
+
+        println 'envers history of Employees:'
+        folks.each {
+            println 'revision:' + it[1].id + ', entity: '+ it[0]
+        }
+
+        then:
+        folks.size() == 4
+    }
+}
--- src/test/groovy/org/javers/organization/structure/JaversQueryTest.groovy
+++ src/test/groovy/org/javers/organization/structure/JaversQueryTest.groovy
@@ -1,7 +1,5 @@
 package org.javers.organization.structure
 
-import org.hibernate.envers.AuditReaderFactory
-import org.hibernate.envers.query.AuditEntity
 import org.javers.core.Javers
 import org.javers.repository.jql.QueryBuilder
 import org.javers.shadow.Shadow
@@ -9,14 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
-import javax.persistence.EntityManager
-import javax.persistence.PersistenceContext
-import javax.transaction.Transactional
-
-import static org.hibernate.envers.RevisionType.MOD
-
 @SpringBootTest
-class QueryingTest extends Specification{
+class JaversQueryTest extends Specification{
     @Autowired
     HierarchyService hierarchyService
 
@@ -25,39 +17,6 @@ class QueryingTest extends Specification{
 
     @Autowired
     Javers javers
-
-    @PersistenceContext
-    EntityManager entityManager
-
-    @Transactional
-    def "should browse Envers history of objects by type"(){
-        given:
-        def gandalf = hierarchyService.initStructure()
-        def aragorn = gandalf.getSubordinate('Aragorn')
-        gandalf.prettyPrint()
-
-        //changes
-        hierarchyService.giveRaise(gandalf, 200)
-        hierarchyService.updateCity(gandalf, 'Shire')
-        hierarchyService.giveRaise(aragorn, 100)
-        hierarchyService.updateCity(aragorn, 'Shire')
-
-        when:
-        List folks = AuditReaderFactory
-                .get(entityManager)
-                .createQuery()
-                .forRevisionsOfEntity( Employee, false, true )
-                .add(AuditEntity.revisionType().eq(MOD)) // filter out initial versions
-                .getResultList()
-
-        println 'envers history of Employees:'
-        folks.each {
-            println 'revision:' + it[1].id + ', entity: '+ it[0]
-        }
-
-        then:
-        folks.size() == 4
-    }
 
     def "should browse JaVers history of objects by type"(){
         given:
@@ -87,9 +46,5 @@ class QueryingTest extends Specification{
         shadows.size() == 4
         shadows[0].commitMetadata.id.majorId == 5
         shadows[3].commitMetadata.id.majorId == 2
-    }
-
-    def setup() {
-        theCleaner.cleanDb()
     }
 }
